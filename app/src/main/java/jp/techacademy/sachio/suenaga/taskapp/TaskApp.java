9d2ce2790ee244ee83cf6a64489311c1d75e9932
sachi <sachio0013@gmail.com>
2018-05-31 22:49:38 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/sachio/suenaga/taskapp/TaskApp.java
@@ -0,0 +1,13 @@
+package jp.techacademy.sachio.suenaga.taskapp;
+
+import android.app.Application;
+
+import io.realm.Realm;
+
+public class TaskApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Realm.init(this);
+    }
+}
